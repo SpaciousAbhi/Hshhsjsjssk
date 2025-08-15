b89022e50ed93bb867ce7f54eeda4e4e2719f057
SpaciousAbhi <74132323+SpaciousAbhi@users.noreply.github.com>
2025-08-15 14:33:08 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,8 +4,8 @@ class Config:
     API_ID = int(getenv("API_ID", "4770590"))
     API_HASH = getenv("API_HASH", "e33bf9032335b874acb9c6406f044836")
     BOT_TOKEN = getenv("BOT_TOKEN", "6926248838:AAEz3ikX6UK7rAW91zhqg_2O-aapbBZaLyQ")
-    FSUB = getenv("FSUB", "")
-    CHID = int(getenv("CHID", ""))
+    FSUB = getenv("FSUB", "Venom_Stone_Movies_Official")
+    CHID = int(getenv("CHID", "-1001409283963"))
     SUDO = list(map(int, getenv("1654334233").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://kajaki7757:kGDRiPGNX691vlCL@cluster0.ylww9nv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     
