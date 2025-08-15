de95e378216c5c21dde7ae6190745514497f3cd9
SpaciousAbhi <74132323+SpaciousAbhi@users.noreply.github.com>
2025-08-15 21:22:40 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -3,7 +3,7 @@ from os import path, getenv
 class Config:
     API_ID = int(getenv("API_ID", "4770590"))
     API_HASH = getenv("API_HASH", "e33bf9032335b874acb9c6406f044836")
-    BOT_TOKEN = getenv("BOT_TOKEN", "6926248838:AAEz3ikX6UK7rAW91zhqg_2O-aapbBZaLyQ")
+    BOT_TOKEN = getenv("BOT_TOKEN", "8484034207:AAEpSsn8-T8FTmfVxAvByU04mw_jkBFz1_w")
     FSUB = getenv("FSUB", "Venom_Stone_Movies_Official")
     CHID = int(getenv("CHID", "-1001409283963"))
     SUDO = list(map(int, getenv("SUDO").split()))
